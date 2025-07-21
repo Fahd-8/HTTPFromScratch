58ffacfd5a6d4d48dbd4680fa1d95d9e9929cc70
Fahad <fahadzaman00000@gmail.com>
2025-07-21 12:32:45 +0500

    Initial commit

---
+++ server.py
@@ -0,0 +1,20 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/hello':
+            # Respond with a custom message for /hello
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"<h1>Hello, World!</h1>")
+        else:
+            # Default behavior for other paths
+            super().do_GET()
+
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
